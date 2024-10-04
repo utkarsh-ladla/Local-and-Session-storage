3f06a09a6eff9baba8c36236b9c524a02e317b86
UTKARSH LADLA <128512468+utkarsh-ladla@users.noreply.github.com>
2024-10-04 22:28:20 +0530

    Local and Session storage

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Local-and-Session-storage
-Build a one component that uses local storage for the simple counter while close the tab repoen the same tab then value of that counter should be last one. Second is a create email and password field alongside button save the value of the both fields on session storage and while close the tab and reopen it it will remove from session storage
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/components/Storage.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+
+function Storage(){
+
+  // Counter using localStorage
+  const [counter, setCounter] = useState(() => {
+    const savedCounter = localStorage.getItem('counter');
+    return savedCounter ? parseInt(savedCounter, 10) : 0;
+  });
+
+  useEffect(() => {
+    // Updating localStorage whenever counter change
+    localStorage.setItem('counter', counter);
+  }, [counter]);
+
+  const [email, setEmail] = useState(() => sessionStorage.getItem('email') || '');
+  const [password, setPassword] = useState(() => sessionStorage.getItem('password') || '');
+
+  const handleSave = () => {
+    sessionStorage.setItem('email', email);
+    sessionStorage.setItem('password', password);
+  };
+
+  // For Clear the form
+  const handleClear = () => {
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('password');
+    setEmail('');
+    setPassword('');
+  };
+
+
+  return (
+    <div style={{ padding: '20px' }}>
+    {/* Counter */}
+    <div>
+      <h2>Counter</h2>
+      <button onClick={() => setCounter(counter + 1)}>Increase</button>
+      <button onClick={() => setCounter(counter - 1)} disabled={counter <= 0}>
+        Decrease
+      </button>
+      <p>Current Counter: {counter}</p>
+    </div>
+
+    {/* form */}
+    <div>
+      <h2>Email & Password</h2>
+      <label>Email: </label>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <label>Password: </label>
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+      <button onClick={handleSave}>Save to Session Storage</button>
+      <button onClick={handleClear}>Clear</button>
+    </div>
+  </div>
+  )
+}
+export default Storage
